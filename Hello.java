d813e15b4fd1ed0a5aab50b74ec42202722c4da7
coltera837 <chozaartem2003@mail.ru>
2023-10-10 23:32:46 +0300

    add A string

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@
 public class Hello {
     public static void main(String[] args)
     {
+	System.out.println("A");
 	System.out.println("1");
         System.out.println("Hello world");
 	System.out.println("2");
